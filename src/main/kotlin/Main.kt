648b2a2e68f3267ef17181a3b5cc9b17b494200e
ma1rcos <mfbdcarvalho@gmail.com>
2024-10-25 01:01:49 -0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,15 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main() = runBlocking {
+    val numbers = IntArray(100000) { it }
+    val target = 99999
+    val search = Search()
+    val binarySearchJob = launch {
+        search.executeBinary(numbers, target)
     }
+    val linearSearchJob = launch {
+        search.executeLinear(numbers, target)
+    }
+    binarySearchJob.join()
+    linearSearchJob.join()
 }
